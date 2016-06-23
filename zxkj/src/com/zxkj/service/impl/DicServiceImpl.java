8862f398a91fba977a3e7a84ab83832c97e67e50
testIcs <guorongbin@chinasoftinc.com>
2016-06-24 00:17:45 +0800

    提交人:雍锋 提交原因:接口代码上传

---
+++ zxkj/src/com/zxkj/service/impl/DicServiceImpl.java
@@ -0,0 +1,51 @@
+package com.zxkj.service.impl;
+
+import java.util.List;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+
+import com.zxkj.common.Constants;
+import com.zxkj.dao.DicMapper;
+import com.zxkj.dao.UserMapper;
+import com.zxkj.model.User;
+import com.zxkj.service.DicService;
+import com.zxkj.service.UserService;
+
+@Scope("prototype")
+@Service("dicService")
+public class DicServiceImpl implements DicService {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(DicServiceImpl.class);
+	
+	@Autowired(required = true)
+	private DicMapper dicMapper;
+	
+	/**
+	 * 查询金融产品字典信息 
+	 */
+	@Override
+	public List<Map<Integer, String>> findFinancialProducts() {
+		return dicMapper.findFinancialProducts();
+	}
+	
+	/**
+	 * 查询贷款利率字典信息 
+	 */
+	@Override
+	public List<Map<Integer, String>> findLendingRate() {
+		return dicMapper.findLendingRate();
+	}
+	
+	/**
+	 * 查询贷款期限字典信息  
+	 */
+	@Override
+	public List<Map<Integer, String>> findLoanTerm() {
+		return dicMapper.findLoanTerm();
+	}
+}
