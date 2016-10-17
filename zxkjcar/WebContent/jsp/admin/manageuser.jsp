623ff5904c5fa07b19f4787fbe76bd53ad5a366e
郭荣斌 <guorongbin@chinasoftinc.com>
2016-10-17 15:51:25 +0800

    22

---
+++ zxkjcar/WebContent/jsp/admin/manageuser.jsp
@@ -0,0 +1,62 @@
+<%@ page contentType="text/html; charset=UTF-8"%>
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+<%
+response.setHeader("Pragma","No-cache");
+response.setHeader("Cache-Control","no-cache");
+response.setDateHeader("Expires", -10);
+%>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>审核用户</title>
+<meta name="keywords" content="">
+<meta name="description" content="">
+<meta charset="utf-8">
+<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+<meta http-equiv="Pragma" content="no-cache" />
+<meta http-equiv="Expires" content="0" />
+<meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no">
+<meta name="apple-mobile-web-app-capable" content="yes">
+<%@ include file="../include/header.jsp" %>
+<style type="text/css">
+	.table td {
+    	text-align: left;
+    	border-right: none;
+	}
+	
+	.container-fluid button{
+		margin:10px 0 0 0;
+	}
+	.table td button{
+		margin:0 5px 0 0 ;
+	}
+</style>
+</head>
+<body>
+<div class="top">注册人员审核</div>
+<div class="container-fluid">
+  <button id="reviewUser" class="btn btn-primary">注册审核</button>
+  <button id="manageUser" class="btn btn-primary">人员管理</button>
+  <button id="addUser" class="btn btn-primary">增加用户</button>
+  <table class="table">
+    <thead>
+        <tr>
+          <th>姓名</th>
+  	      <th>手机号</th>
+  	      <th>操作</th>
+        </tr>
+    </thead>
+    <tbody>
+  	  
+    </tbody>
+  </table>
+</div>
+</body>
+<%@ include file="../include/footer.jsp" %>
+<script type="text/javascript" src="../../script/manageuser.js"></script>
+<script type="text/javascript">
+	$(function(){
+		AuditUserModule.init();
+	})
+</script>
+</html>
---
+++ zxkjcar/WebContent/script/admin/manageuser.js
@@ -0,0 +1,131 @@
+﻿window.AuditUserModule = (function($, module)
+{
+	
+	//初始化数据
+	function initData()
+    {
+		
+		$.ajax({    
+			url:'/zxkjcar/admin/queryBeAuditedUser.do',       
+			type:'post',    
+			cache:false,  			
+			dataType:'json', 
+			beforeSend: function () {
+				$.showLoadding();
+		    },
+		    success: function (data) {
+				if(data)
+				{
+					render(data);
+				}	
+		    },
+		    complete: function () {
+		    	$.hideLoadding();
+		    },
+		    error: function (data) {
+		    	$.hideLoadding();
+		        console.info("error: " + data.responseText);
+		    }
+
+		});
+	}
+	
+	//渲染数据
+	function render(userList){
+		$(".table tbody").empty()
+		if(userList.length==0)
+		{
+			$(".table tbody").append("<tr><td colspan='3' style='text-align:center'>暂时无数据</td></tr>");
+			return;
+		}
+		$datahtml = "";
+		$.each(userList,function(i,user){
+			$datahtml +="<tr>";
+			$datahtml +=  "<td>"+user.userName+"</td>";
+			$datahtml +=  "<td>"+user.phoneNo+"</td>";
+			$datahtml +=  "<td>";
+			$datahtml +=    "<button type='button' class='btn btn-primary btn-xs' onclick='AuditUserModule.passed("+user.userId+")'>通过</button>"
+			$datahtml +=    "<button type='button' class='btn btn-default btn-xs' onclick='AuditUserModule.deleteUser("+user.userId+")'>删除</button>"
+			$datahtml +=  "</td>";
+			$datahtml += "</tr>"
+		});
+		$(".table tbody").append($datahtml);
+	}
+	
+	//审核通过
+	function passed(id){
+		$.ajax( {    
+			url:'/zxkjcar/admin/auditUser.do',// 跳转到 action        
+			type:'post',    
+			cache:false,  			
+			dataType:'json', 
+			data:{id:id,state:1}
+		}).done(function(flag){
+			if(flag=='0')
+			{
+				jAlert("审核成功","提示");
+				initData();
+			}
+			else
+			{
+				jAlertError("审核失败","提示");
+			}
+		}).fail(function(){
+
+		});
+	}
+	
+	//审核通过
+	function deleteUser(id){
+		jConfirm("确认删除吗？","提示",function(r){
+			if(!r)
+			{
+				return;
+			}
+			$.ajax( {    
+				url:'/zxkjcar/admin/deleteUser.do',// 跳转到 action        
+				type:'post',    
+				cache:false,  			
+				dataType:'json', 
+				data:{id:id},
+				success:function(flag){
+					if(flag=='0')
+					{
+						jAlert("删除成功","提示");
+						initData();
+					}
+					else
+					{
+						jAlertError("删除失败","提示");
+					}
+				},
+				error: function (data) {
+			        console.info("error: " + data.responseText);
+			    }
+			});
+		})
+	}
+	
+	/**
+	 * 为按钮绑定事件
+	 */
+	function bindEventForButton()
+	{
+		$("#addUser").on("click", function(){
+			window.location.href='adduser.jsp';
+		});
+	}
+	
+	function init()
+	{
+		bindEventForButton();
+		initData();
+	}
+	
+	module.init = init;
+	module.passed = passed;
+	module.deleteUser = deleteUser;
+	
+	return module;
+	
+})($, window.AuditUserModule || {});
